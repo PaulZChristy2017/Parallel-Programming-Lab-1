138c8a041185cf10d06afbea56ec755f24b234e6
PaulZChristy2017 <paul.christy2017@gmail.com>
2019-03-14 21:32:18 -0500

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/PP.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PP.dir/main.c.o"
+  "PP.pdb"
+  "PP.exe"
+  "libPP.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
